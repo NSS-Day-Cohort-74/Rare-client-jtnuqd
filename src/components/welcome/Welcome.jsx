630950cbf05bb3445f24aabc38c6735924f60d29
Sabra Kozlina <sjkozlina@gmail.com>
2025-03-04 14:56:33 -0700

    added welcome page

---
+++ src/components/welcome/Welcome.jsx
@@ -0,0 +1,12 @@
+import "./Welcome.css"
+
+export const Welcome = () => {
+    return (
+      <div className="welcome-container">
+        <h1>
+          <span>Meal Prep</span>
+        </h1>
+        <div>Affordable weekly meals</div>
+      </div>
+    );
+  };
