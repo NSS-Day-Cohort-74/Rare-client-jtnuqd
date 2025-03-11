8681eb77a7443b2d5f4575b34116aa6ae8f5e071
Nate Z <ntzeitlin@gmail.com>
2025-03-11 11:01:33 -0500

    Subscription button added, disabled if current user is looking at their own profile, or if they are already subscribed to the user

--- src/components/users/UserDetails.jsx
+++ src/components/users/UserDetails.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import { getUserDataByUserId } from "../../services/userService";
 import { HumanDate } from "../utils/HumanDate";
 
-export const UserDetail = () => {
+export const UserDetail = ({ token }) => {
     const [userData, setUserData] = useState();
 
     const { userId } = useParams();
@@ -13,27 +13,44 @@ export const UserDetail = () => {
     }, []);
 
     return (
-        <div className="card p-2 m-5">
-            <section className="columns m-2 p-2">
-                <div className="column card-image">
-                    <figure className="image-is4by3">
-                        <img
-                            src={userData?.profile_image_url}
-                            alt="Placeholder img"
-                        />
-                    </figure>
-                    <div>
-                        Full Name: {userData?.first_name} {userData?.last_name}
-                    </div>
+        <div className="grid">
+            <div className="cell is-col-min-20">
+                <div className="card p-2 m-5">
+                    <section className="columns m-2 p-2">
+                        <div className="column card-image">
+                            <figure className="image-is4by3">
+                                <img
+                                    src={userData?.profile_image_url}
+                                    alt="Placeholder img"
+                                />
+                            </figure>
+                            <div>
+                                Full Name: {userData?.first_name}{" "}
+                                {userData?.last_name}
+                            </div>
+                        </div>
+                        <div className="column">
+                            <div>Username: {userData?.username}</div>
+                            <div>
+                                Joined:{" "}
+                                <HumanDate date={userData?.created_on} />
+                            </div>
+                            <div>Bio: {userData?.bio}</div>
+                        </div>
+                    </section>
                 </div>
-                <div className="column">
-                    <div>Username: {userData?.username}</div>
-                    <div>
-                        Joined: <HumanDate date={userData?.created_on} />
-                    </div>
-                    <div>Bio: {userData?.bio}</div>
-                </div>
-            </section>
+            </div>
+            <div className="cell">
+                {userData?.followers?.includes(parseInt(token)) ||
+                parseInt(token) === parseInt(userId) ? (
+                    <button className="button" disabled>
+                        Subscribe to User
+                    </button>
+                ) : (
+                    <button className="button">Subscribe to User</button>
+                )}
+                {/* <button className="button">Loading...</button> */}
+            </div>
         </div>
     );
 };
