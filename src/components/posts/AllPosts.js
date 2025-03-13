3a47ba675f5a3faca9b7af7b19776a98ed6d12c4
Darren Raines <draines.123123@gmail.com>
2025-03-13 10:13:54 -0500

    Implemented the ability for a user to filter post by tag

--- src/components/posts/AllPosts.js
+++ src/components/posts/AllPosts.js
@@ -7,6 +7,9 @@ export const AllPosts = () => {
     const [allPosts, setAllPosts] = useState([])
     const [allCategories, setAllCategories] = useState([])
     const [selectedCategory, setSelectedCategory] = useState("")
+    const [allTags, setAllTags] = useState([])
+    const [selectedTag, setSelectedTag] = useState("")
+    const [postTags, setAllPostTags] = useState([])
     const [error, setError] = useState(null)
 
     useEffect(()=> {
@@ -20,16 +23,42 @@ export const AllPosts = () => {
         fetch("http://localhost:8088/categories")
         .then(response => response.json())
         .then(data => setAllCategories(data))
+        .catch(error => console.error("Error with fetching categories", error))
+    }, [])
+
+    useEffect(()=> {
+        fetch("http://localhost:8088/tags")
+        .then(response => response.json())
+        .then(data => setAllTags(data))
         .catch(error => console.error("Error with fetching tags", error))
+    }, [])
+    
+    useEffect(() => {
+        fetch("http://localhost:8088/posttags")
+        .then(response => response.json())
+        .then(data => setAllPostTags(data))
+        .catch(error => console.error("Error with fetching posttags", error))
     }, [])
 
     const handleCategoryChange = (event) => {
         setSelectedCategory(event.target.value)
     }
 
-    const filteredPosts = selectedCategory ? allPosts.filter(post => 
-        post.category_id === parseInt(selectedCategory)) : allPosts
+    const handleTagChange = (event) => {
+        setSelectedTag(event.target.value)
+    }
 
+    const filteredPosts = allPosts.filter(post => {
+        if (!postTags.length) return true;
+        
+        const categoryMatch = selectedCategory ? post.category_id === parseInt(selectedCategory) : true;
+        const tagMatch = selectedTag ? postTags.some(pt => pt.post_id === post.id && pt.tag_id === parseInt(selectedTag)) : true;
+        return categoryMatch && tagMatch
+    })
+
+    console.log("postTags", postTags)
+    console.log("selectedTag", selectedTag)
+    
     return (
         <section>
             <h1 className="title is-3 has-text-centered">All Posts</h1>
@@ -40,6 +69,17 @@ export const AllPosts = () => {
                         <option value="">All Categories</option>
                         {allCategories.map(category => (
                             <option key={category.id} value={category.id}>{category.label}</option>
+                        ))}
+                    </select>
+                </div>
+            </div>
+            <div className="m-3 level-item">
+                <label>Filter by Tag: </label>
+                <div className="m-3">
+                    <select value={selectedTag} onChange={handleTagChange} >
+                        <option value="">All Tag</option>
+                        {allTags.map(tag => (
+                            <option key={tag.id} value={tag.id}>{tag.label}</option>
                         ))}
                     </select>
                 </div>
