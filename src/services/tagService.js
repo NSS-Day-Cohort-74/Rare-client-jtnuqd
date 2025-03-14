15bde844d15f6d9735a22de04b6963bd7f1a3e58
Nate Z <assured.toady_0n@icloud.com>
2025-03-14 13:33:07 -0500

    Revert "Ft/filter by user"

--- src/services/tagService.js
+++ src/services/tagService.js
@@ -1,31 +1,9 @@
 export const createNewTag = async (submissionObject) => {
-  await fetch("http://localhost:8088/tags", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(submissionObject),
-  });
-};
-
-export const getPostTags = (postId) => {
-  return fetch(`http://localhost:8088/tags/posts/${postId}`).then((response) =>
-    response.json()
-  );
-};
-
-export const deletePostTags = (postId) => {
-  return fetch(`http://localhost:8088/tags/posts/${postId}`, {
-    method: "DELETE",
-  }).then((response) => response.json());
-};
-
-export const updatePostTags = (postId, tagIds) => {
-  return fetch(`http://localhost:8088/tags/posts/${postId}`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ tag_ids: tagIds }),
-  }).then((response) => response.json());
-};
+    await fetch("http://localhost:8088/tags", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(submissionObject)
+    })
+}
